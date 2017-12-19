89e448b296a185b119ca8683268600fb9b3a1bf2
james lawson <jlawson079@gmail.com>
2017-12-19 14:53:52 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+//lesson 1
+// //pring 'hello world' to the console
+// 	console.log("hello world")
+// 	//print 'hell world' to the web page (AKA, the document)
+// 	document.write("hello world")
+// 	//prints a message in a pop up box
+// 	alert("hello world")
+
+//lesson 2
+// var name = prompt("What's your name?")
+// console.log(name)
+
+//lesson 3
+// var numberValue = 12
+// var numberValue2 = Number(prompt("choose a number between 1 - 100"))
+// var booleanValue = true
+// var stringValue = "Hello World"
+// var arrayValue = [33, false, "string"]
+
+//lesson 4: contatenation
+// var combineVariables = numberValue + numberValue2
+// console.log(combineVariables)
+
+// lesson 5: if/else
+// if(numberValue > numberValue2) {
+// 	alert(numberValue + " is greater than " + numberValue2)
+// } else if (numberValue < numberValue2) {
+// 	alert(numberValue2 + " is greater than " + numberValue)
+// } else {
+// 	alert(numberValue + " is equal to " + numberValue2)
+// }
+
+// lesson 6: comparison and logical operators 
+// var num1 = 6
+// var num2 = 10
+// // = assings a calue to a varialbe
+// // == tests to see if two values are equal
+// // === tests to see if two values are equal and are the same type
+// if(num1 == 6 || num2 == 100) {
+// 	console.log("true")	
+// } else {
+// 	console.log("false")
+// }
+
+// lesson 7: looping
+// While Loop:
+// var username = prompt("What's your name?")
+
+// while(username != "James") {
+// 	username = prompt("What's your name?")
+// }
+
+// For Loop:
+// for(var i = 1 ;i <= 20; i++) {
+// 	document.write(i)
+// }
+
+// lesson 8: functions
+var num1 = 10
+var num2 = 15
+
+// name of function is 'add'
+function add(a) {
+	document.write(a * a)
+}
+
+add(num1)
+
+
+
+
+
+
+
+
+
+
